9e2161d7eaf44c76df5f62282f5bd5319cff0db8
hex <idontknowbrotherpls@gmail.com>
2022-04-09 23:49:27 +0700

    done: final multi-media

--- scripts/game.js
+++ scripts/game.js
@@ -4,19 +4,193 @@ var player = {
     score: 0,
     hitted: false,
     missTotal: 0,
-    balls: []
+    balls: [],
+    lastPositionHit: {
+        top: 0,
+        left: 0
+    }
 }
+var arrayGrid = [
+    {
+        top: 5,
+        left: 5
+    },
+    {
+        top: 20,
+        left: 5
+    },
+    {
+        top: 35,
+        left: 5
+    },
+    {
+        top: 50,
+        left: 5
+    },
+    {
+        top: 65,
+        left: 5
+    },
+    {
+        top: 80,
+        left: 5
+    },
+    {
+        top: 5,
+        left: 20
+    },
+    {
+        top: 20,
+        left: 20
+    },
+    {
+        top: 35,
+        left: 20
+    },
+    {
+        top: 50,
+        left: 20
+    },
+    {
+        top: 65,
+        left: 20
+    },
+    {
+        top: 80,
+        left: 20
+    },
+    {
+        top: 5,
+        left: 35
+    },
+    {
+        top: 20,
+        left: 35
+    },
+    {
+        top: 35,
+        left: 35
+    },
+    {
+        top: 50,
+        left: 35
+    },
+    {
+        top: 65,
+        left: 35
+    },
+    {
+        top: 80,
+        left: 35
+    },
+    {
+        top: 5,
+        left: 50
+    },
+    {
+        top: 20,
+        left: 50
+    },
+    {
+        top: 35,
+        left: 50
+    },
+    {
+        top: 50,
+        left: 50
+    },
+    {
+        top: 65,
+        left: 50
+    },
+    {
+        top: 80,
+        left: 50
+    },
+    {
+        top: 5,
+        left: 65
+    },
+    {
+        top: 20,
+        left: 65
+    },
+    {
+        top: 35,
+        left: 65
+    },
+    {
+        top: 50,
+        left: 65
+    },
+    {
+        top: 65,
+        left: 65
+    },
+    {
+        top: 80,
+        left: 65
+    },
+    {
+        top: 5,
+        left: 80
+    },
+    {
+        top: 20,
+        left: 80
+    },
+    {
+        top: 35,
+        left: 80
+    },
+    {
+        top: 50,
+        left: 80
+    },
+    {
+        top: 65,
+        left: 80
+    },
+    {
+        top: 80,
+        left: 80
+    },
+]
 
 const lvSettings = {
     1: {
         levelName: "Grid Shot Normal Mode",
         numBalls: 6,
         isBallMovement: false,
-        maxTime: 3,
+        maxTime: 90,
         missScore: 120,
-        requestScore: 10,
-        hitScore: 173,
+        requestScore: 30000,
+        hitScore: 185,
         ballSize : 80,
+        ballColor : 'aqua',
+        background : 'grey'
+    },
+    2:  {
+        levelName: "Grid Shot Hard Mode",
+        numBalls: 6,
+        isBallMovement: false,
+        maxTime: 90,
+        missScore: 120,
+        requestScore: 30000,
+        hitScore: 185,
+        ballSize : 40,
+        ballColor : 'aqua',
+        background : 'grey'
+    },
+    3:  {
+        levelName: "Grid Shot Normal Mode",
+        numBalls: 6,
+        isBallMovement: false,
+        maxTime: 121,
+        missScore: 222,
+        requestScore: 50000,
+        hitScore: 185,
+        ballSize : 40,
         ballColor : 'aqua',
         background : 'grey'
     }
@@ -58,6 +232,7 @@ function startTimer(config, display) {
         display.innerHTML = `Time: <span style color="black"> ${minutes}:${seconds} </span>`;
 
         if (--timer < 0) {
+
             if(player.score < config.requestScore){
                 failedPage(config)
                 return
@@ -69,39 +244,32 @@ function startTimer(config, display) {
 }
 
 function randonPositionBall(){
-    var isSuccess = true
-    var position
+    var position = arrayGrid[getRndInteger(0, arrayGrid.length)]
 
     if(player.balls.length < 1){
-        return {
-            left: randomInt(70),
-            top: randomInt(70)
+        return position
+    }
+
+    for(let iv=0; iv < player.balls.length; iv++){
+        let ballCheck = player.balls[iv]
+        if(`${position.left}%` == ballCheck.style.left && `${position.top}%` == ballCheck.style.top){
+            return false
         }
     }
 
-    do{
-        position = {
-            left: randomInt(70),
-            top: randomInt(70)
-        }
-
-        player.balls.forEach(ball => {
-            if(
-                position.left == ball.style.left && position.top == ball.style.top
-            ){
-                isSuccess == false
-                return
-            }
-        })
-
-    }while(!isSuccess)
-
     return position
+
 }
 
 function refreshScore(){
     var scoreLabel = document.getElementById('score')
     scoreLabel.innerHTML = `Level: <span style="color: red">${player.lv}</span> Score: <span style="color: gold">${player.score}</span>`
+    player.balls.forEach(ball=>{
+        console.log({
+            top: ball.style.top,
+            left: ball.style.left
+        });
+    })
 }
 
 function missShot(config){
@@ -117,16 +285,21 @@ function missShot(config){
 function hitShot(config, ball){
     player.hitted = true
     player.score += config.hitScore
-    
+    player.lastPositionHit = {
+        top: ball.style.top,
+        left: ball.style.left
+    }
 
     const ballIndex = player.balls.indexOf(ball)
     if(ballIndex > -1){
         player.balls.splice(ballIndex, 1)
     }
     ball.style.background = 'red';
+    
     setTimeout(function(){
         ball.remove()
-    }, 30)
+    }, 15)
+    
     addNewBall(config)
     refreshScore()
     
@@ -138,7 +311,10 @@ function addNewBall(config){
     ball.classList.add("ball");
     ball.style.background = config.ballColor
 
-    const positionBall = randonPositionBall(config.ballSize)
+    let positionBall = randonPositionBall()
+    while(positionBall == false){
+        positionBall = randonPositionBall()
+    }
 
     ball.style.left = `${positionBall.left}%`
     ball.style.top = `${positionBall.top}%`
@@ -175,6 +351,10 @@ function randomSpawnTarget(lv){
 }
 
 function resetPlayer(){
+    if(timeCounter != 0){
+        clearInterval(timeCounter)
+        timeCounter = 0
+    }
     player.score = 0
     player.hitted = false
     player.balls = []
@@ -199,7 +379,6 @@ function startAim(lv){ // เริ่มเกม
 }
 
 function successPage(config){
-    clearInterval(timeCounter)
 
     var window = document.getElementsByClassName('main-container')[0]
     player.lv++;
@@ -223,7 +402,6 @@ function successPage(config){
 }
 
 function failedPage(config){
-    clearInterval(timeCounter)
 
     var window = document.getElementsByClassName('main-container')[0]
     window.innerHTML = `
